1bb4eafc0aaea8f2ce0bfc97b4737d95f7d2383c
Jason Lothamer <jhlothamer@gmail.com>
2022-10-10 18:48:20 -0400

    add witch model and animations.  Change witch and base character to enable playing the right animation per state/situation

--- game/scenes/characters/witch.gd
+++ game/scenes/characters/witch.gd
@@ -1,3 +1,47 @@
 class_name Witch
 extends Character
 
+onready var _animation_player: AnimationPlayer = $WitchAnimations_Frank_Ilikethepixies/AnimationPlayer
+
+
+var _state_animations := {
+	"Idle": "Idle",
+	"Walk": "Walking",
+	"Shoot": "Shoot Magic",
+}
+
+var _looping_animations := [
+	"Idle",
+	"Walking",
+	"Cauldron",
+	"Crystal Ball",
+	"Manapool"
+]
+
+var _interactable_object_name := ""
+
+var _interact_state_animations_for_object := {
+	"Cauldron": "Cauldron",
+	"ManaPool": "Manapool",
+	"CrystalBall": "Crystal Ball",
+}
+
+
+func _on_StateMachine_state_changed(old_state, new_state):
+	._on_StateMachine_state_changed(old_state, new_state)
+	if new_state == "Interact":
+		if _interact_state_animations_for_object.has(_interactable_object_name):
+			_animation_player.play(_interact_state_animations_for_object[_interactable_object_name])
+	if _state_animations.has(new_state):
+		_animation_player.play(_state_animations[new_state])
+
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if _looping_animations.has(anim_name):
+		_animation_player.play(anim_name)
+
+
+func _on_Walk_interaction_about_to_start(interactable_object: Node):
+	_interactable_object_name = interactable_object.name
+
--- game/scenes/characters/witch_states/witch_shoot.gd
+++ game/scenes/characters/witch_states/witch_shoot.gd
@@ -4,40 +4,57 @@ extends CharacterBaseState
 const SCENE_MAGIC_MISSILE = preload("res://scenes/projectiles/magic_missile.tscn")
 
 export var deny_interaction_sound: NodePath
-
+export var fire_from_position: NodePath
+export var delay_before_firing := .7
+export var delay_before_shooting_again := 1.0
 
 onready var _deny_interaction_sound: AudioStreamPlayer3D = get_node_or_null(deny_interaction_sound)
-
+onready var _fire_from_position: Position3D = get_node(fire_from_position)
 
 var _target_enemy: Enemy
-
+var _timer := 0.0
+var _have_fired := false
+var _target_pos: Vector3
+var _firing := false
 
 func _ready():
 	SignalMgr.register_subscriber(self, "enemy_clicked")
 
 
 func _on_enemy_clicked(enemy: CollisionObject) -> void:
-	if !character.is_selected():
+	if !character.is_selected() or _firing:
 		return
-	var witch: Witch = character
-	if !witch.has_required_resource_amount(GameConsts.RESOURCE_MANA, Projectile.MANA_USED):
+	if !character.has_required_resource_amount(GameConsts.RESOURCE_MANA, Projectile.MANA_USED):
 		if _deny_interaction_sound and !_deny_interaction_sound.is_playing():
 			_deny_interaction_sound.play()
 		return
-	witch.decrease_resource_amount(GameConsts.RESOURCE_MANA, Projectile.MANA_USED)
+	character.decrease_resource_amount(GameConsts.RESOURCE_MANA, Projectile.MANA_USED)
 	_target_enemy = enemy
 	change_state(name)
+	_firing = true
 
 
 func enter() -> void:
-	var target_pos: Vector3 = _target_enemy.global_transform.origin
-	character.look_at(target_pos, Vector3.UP)
+	_target_pos = _target_enemy.global_transform.origin
+	character.look_at(_target_pos, Vector3.UP)
+	_timer = 0.0
+	_have_fired = false
+
+
+func physics_process(delta):
+	_timer += delta
+	
+	if _timer > delay_before_firing and !_have_fired:
+		_spawn_projectile()
+		_have_fired = true
+	if _timer >= delay_before_shooting_again:
+		change_state("Idle")
+		_firing = false
+
+
+func _spawn_projectile() -> void:
 	var mm = SCENE_MAGIC_MISSILE.instance()
 	mm.add_collision_exception_with(character)
 	GameUtil.get_dynamic_parent(character).add_child(mm)
-	mm.global_transform.origin = character.global_transform.origin + Vector3.UP*.5
-	mm.set_target_position(target_pos)
-	change_state("Idle")
-
-
-
+	mm.global_transform.origin = _fire_from_position.global_transform.origin
+	mm.set_target_position(_target_pos)
