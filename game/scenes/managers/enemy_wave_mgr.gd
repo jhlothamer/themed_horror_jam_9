33bbcc2babc7e89f85d3b69c9308df95b8d6f3eb
Jason Lothamer <jhlothamer@gmail.com>
2022-10-11 13:23:06 -0400

    add light to lighten characters. turn off constant outline.

--- game/scenes/managers/enemy_wave_mgr.gd
+++ game/scenes/managers/enemy_wave_mgr.gd
@@ -2,6 +2,7 @@ class_name EnemyWaveMgr
 extends Node
 
 export var delay_between_enemy_spawns := 1.0
+export var disabled := false
 
 var _enemy_spawn_counter := 1
 var _enemy_increase_amount := 0.0
@@ -11,6 +12,8 @@ func _on_Timer_timeout():
 	_spawn_wave()
 
 func _spawn_wave() -> void:
+	if disabled:
+		return
 	var spawn_mgr:EnemySpawnMgr = ServiceMgr.get_service(EnemySpawnMgr)
 	for i in _enemy_spawn_counter:
 		spawn_mgr.spawn_enemy()
