e9c8937c5bfb1dd3848b57063539197bd5e6637d
Jason Lothamer <jhlothamer@gmail.com>
2022-10-12 05:02:00 -0400

    add door/window break sounds, crystal ball desk, fix witch shoot bug, tweak current enemy wave mgr - add more settings, rand delay between spawns/waves, etc

--- game/scenes/characters/witch_states/witch_shoot.gd
+++ game/scenes/characters/witch_states/witch_shoot.gd
@@ -7,9 +7,11 @@ export var deny_interaction_sound: NodePath
 export var fire_from_position: NodePath
 export var delay_before_firing := .7
 export var delay_before_shooting_again := 1.0
+export var shoot_sound: NodePath
 
 onready var _deny_interaction_sound: AudioStreamPlayer3D = get_node_or_null(deny_interaction_sound)
 onready var _fire_from_position: Position3D = get_node(fire_from_position)
+onready var _shoot_sound: AudioStreamPlayer3D = get_node_or_null(shoot_sound)
 
 var _target_enemy: Enemy
 var _timer := 0.0
@@ -39,6 +41,11 @@ func enter() -> void:
 	character.look_at(_target_pos, Vector3.UP)
 	_timer = 0.0
 	_have_fired = false
+	_firing = false
+
+
+func exit() -> void:
+	_firing = false
 
 
 func physics_process(delta):
@@ -58,3 +65,5 @@ func _spawn_projectile() -> void:
 	GameUtil.get_dynamic_parent(character).add_child(mm)
 	mm.global_transform.origin = _fire_from_position.global_transform.origin
 	mm.set_target_position(_target_pos)
+	if _shoot_sound and !_shoot_sound.is_playing():
+		_shoot_sound.play()
