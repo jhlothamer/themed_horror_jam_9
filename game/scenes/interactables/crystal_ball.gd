52fb46c2604c40025d97f4dd46e4e61014bf55db
Jason Lothamer <jhlothamer@gmail.com>
2022-10-20 09:54:55 -0400

    show crystal ball rune animation only when a ward is active

--- game/scenes/interactables/crystal_ball.gd
+++ game/scenes/interactables/crystal_ball.gd
@@ -7,18 +7,27 @@ onready var _active_indicator_anim_player: AnimationPlayer = $ActiveIndicator/An
 
 
 func _ready():
-	SignalMgr.register_publisher(self, "crystal_ball_status_changed")
 	_active_indicator.visible = false
 	_active_indicator_anim_player.stop()
+	SignalMgr.register_publisher(self, "crystal_ball_status_changed")
+	SignalMgr.register_subscriber(self, "ward_activated")
+	SignalMgr.register_subscriber(self, "ward_deactivated")
 
 
 func _on_InteractionHelper_interaction_started(_interactor):
 	emit_signal("crystal_ball_status_changed", true)
-	_active_indicator.visible = true
-	_active_indicator_anim_player.play("CircleAction001")
 
 
 func _on_InteractionHelper_interaction_interrupted(_interactor):
 	emit_signal("crystal_ball_status_changed", false)
+
+
+func _on_ward_activated():
+	_active_indicator.visible = true
+	_active_indicator_anim_player.play("CircleAction001")
+
+
+func _on_ward_deactivated():
 	_active_indicator.visible = false
 	_active_indicator_anim_player.stop()
+
